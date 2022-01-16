009975d569e236ca3fa2b24f25ed2e8c83fb29fa
Gunn Hwang <gunhwang99@gmail.com>
2022-01-15 21:37:38 -0800

    Remove server.js

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'de7d7e808350a52c1e61cb0ede18b88b'),
-    },
-  },
-});
